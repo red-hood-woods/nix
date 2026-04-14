940cb7c17d515d4c2b07a8492064eae68c4855c4
Alice Liddel <sheepos@proton.me>
2026-04-14 09:51:09 +0000

    git

--- hosts/wonderland/configuration.nix
+++ hosts/wonderland/configuration.nix
@@ -92,7 +92,17 @@
 
     ];
   };
-
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+  ];
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.permittedInsecurePackages = [
